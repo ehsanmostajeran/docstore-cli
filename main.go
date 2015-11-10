db2283d9d6479413746a9e5a86360a0199d05d7e
Thomas Sileo <thomas.sileo@gmail.com>
2015-11-10 10:55:10 +0100

    added more commands

--- main.go
+++ main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/tsileo/docstore-client"
 	"gopkg.in/readline.v1"
 )
 
 // FIXME(ts) add history support
 
 var completer = readline.NewPrefixCompleter(
-	readline.PcItem("say",
-		readline.PcItem("hello"),
-		readline.PcItem("bye"),
-	),
+	// readline.PcItem("say",
+	// 	readline.PcItem("hello"),
+	// 	readline.PcItem("bye"),
+	// ),
+	readline.PcItem("query"),
+	readline.PcItem("get"),
+	readline.PcItem("collection"),
 	readline.PcItem("help"),
 )
 
 func main() {
+	dstore := docstore.New("")
 	rl, err := readline.NewEx(&readline.Config{
 		Prompt:       "docstore > ",
 		AutoComplete: completer,
@@ -23,12 +32,50 @@ func main() {
 		panic(err)
 	}
 	defer rl.Close()
-
+	scol := ""
+	var col *docstore.Collection
 	for {
 		line, err := rl.Readline()
 		if err != nil { // io.EOF
 			break
 		}
-		println(line)
+		switch {
+		case strings.HasPrefix(line, "collection"):
+			args := strings.Split(line, " ")
+			// A col is specified
+			if len(args) == 2 {
+				scol = args[1]
+				col = dstore.Col(scol)
+			}
+			println(scol)
+		case strings.HasPrefix(line, "get"):
+			args := strings.Split(line, " ")
+			if len(args) < 2 {
+				println("no ID provided")
+			}
+			id := args[1]
+			res := map[string]interface{}{}
+			if err := col.Get(id, &res); err != nil {
+				fmt.Printf("error: %v\n", err)
+			}
+			js, _ := json.MarshalIndent(&res, "", "    ")
+			fmt.Printf("%s\n", js)
+		case strings.HasPrefix(line, "insert"):
+			payload := strings.Replace(line, "insert ", "", 1)
+			doc := map[string]interface{}{}
+			if err := json.Unmarshal([]byte(payload), &doc); err != nil {
+				fmt.Printf("error: %s\n", err)
+			}
+			if err := col.Insert(&doc, nil); err != nil {
+				fmt.Printf("error: %s\n", err)
+			}
+			js, _ := json.MarshalIndent(&doc, "", "    ")
+			fmt.Printf("%s\n", js)
+		case strings.HasPrefix(line, "query"):
+
+		default:
+			println(line)
+		}
+		// println(line)
 	}
 }
